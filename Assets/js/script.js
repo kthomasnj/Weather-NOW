cdead395d0d18cd83568db227b81f124f7b7cf93
Keith S Thomas <kthomasnj@hotmail.com>
2022-08-13 12:50:49 -0400

    Added 5-Day forecast functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -52,37 +52,80 @@ submitBtn.addEventListener('click', function () {
             weatherTemp1El.textContent = "Temperture: " + data.list[0].main.temp;
             weatherWind1El.textContent = "Wind: " + data.list[0].wind.speed;
             weatherHum1El.textContent = "Humidity: " + data.list[0].main.humidity;
-            weatherUv1El.textContent = "UV Index: " + data.list[0].weather[0].description;
+            // weatherUv1El.textContent = "UV Index: " + data.list[0].weather[0].id
 
             // Five-Day Forecast
             //Day 1
 
-            weatherCond1El.textContent = "Weather Conditions: " + data.list[0].weather[0].description;
-            weatherTemp1El.textContent = "Temperture: " + data.list[0].main.temp;
-            weatherWind1El.textContent = "Wind: " + data.list[0].wind.speed;
-            weatherHum1El.textContent = "Humidity: " + data.list[0].main.humidity;
-            weatherUv1El.textContent = "UV Index: " + data.list[0].weather[0].description;
+            var day1DateEl = document.querySelector("#day-1 h2");
+            var day1IconEl = document.querySelector("#day-1 div i");
+            var day1TempEl = document.querySelector("#one-temp");
+            var day1WindEl = document.querySelector("#one-wind");
+            var day1HumidEl = document.querySelector("#one-humid");
 
+            day1DateEl.textContent = data.list[0].dt_txt;
+            day1IconEl.textContent = "icon: " + data.list[0].weather[0].icon;
+            day1TempEl.textContent = "Temperture: " + data.list[0].main.temp;
+            day1WindEl.textContent = "Wind: " + data.list[0].wind.speed;
+            day1HumidEl.textContent = "Humidity: " + data.list[0].main.humidity;
+
+            //Day 2
+
+            var day1DateEl = document.querySelector("#day-2 h2");
+            var day1IconEl = document.querySelector("#day-2 div i");
+            var day1TempEl = document.querySelector("#two-temp");
+            var day1WindEl = document.querySelector("#two-wind");
+            var day1HumidEl = document.querySelector("#two-humid");
+
+            day1DateEl.textContent = data.list[9].dt_txt;
+            day1IconEl.textContent = "icon: " + data.list[9].weather[0].icon;
+            day1TempEl.textContent = "Temperture: " + data.list[9].main.temp;
+            day1WindEl.textContent = "Wind: " + data.list[9].wind.speed;
+            day1HumidEl.textContent = "Humidity: " + data.list[9].main.humidity;
+
+            //Day 3
+
+            var day1DateEl = document.querySelector("#day-3 h2");
+            var day1IconEl = document.querySelector("#day-3 div i");
+            var day1TempEl = document.querySelector("#three-temp");
+            var day1WindEl = document.querySelector("#three-wind");
+            var day1HumidEl = document.querySelector("#three-humid");
+
+            day1DateEl.textContent = data.list[18].dt_txt;
+            day1IconEl.textContent = "icon: " + data.list[18].weather[0].icon;
+            day1TempEl.textContent = "Temperture: " + data.list[18].main.temp;
+            day1WindEl.textContent = "Wind: " + data.list[18].wind.speed;
+            day1HumidEl.textContent = "Humidity: " + data.list[18].main.humidity;
+
+            //Day 4
+
+            var day1DateEl = document.querySelector("#day-4 h2");
+            var day1IconEl = document.querySelector("#day-4 div i");
+            var day1TempEl = document.querySelector("#four-temp");
+            var day1WindEl = document.querySelector("#four-wind");
+            var day1HumidEl = document.querySelector("#four-humid");
+
+            day1DateEl.textContent = data.list[27].dt_txt;
+            day1IconEl.textContent = "icon: " + data.list[27].weather[0].icon;
+            day1TempEl.textContent = "Temperture: " + data.list[27].main.temp;
+            day1WindEl.textContent = "Wind: " + data.list[27].wind.speed;
+            day1HumidEl.textContent = "Humidity: " + data.list[27].main.humidity;
+
+            //Day 5
+
+            var day1DateEl = document.querySelector("#day-5 h2");
+            var day1IconEl = document.querySelector("#day-5 div i");
+            var day1TempEl = document.querySelector("#five-temp");
+            var day1WindEl = document.querySelector("#five-wind");
+            var day1HumidEl = document.querySelector("#five-humid");
+
+            day1DateEl.textContent = data.list[36].dt_txt;
+            day1IconEl.textContent = "icon: " + data.list[36].weather[0].icon;
+            day1TempEl.textContent = "Temperture: " + data.list[36].main.temp;
+            day1WindEl.textContent = "Wind: " + data.list[36].wind.speed;
+            day1HumidEl.textContent = "Humidity: " + data.list[36].main.humidity;
         });
 
 });
 
 timeEl.textContent = currentTime;
-
-// function fetchWeather() {
-
-
-//     console.log(cityLonAttr);
-
-//     fetch(currentWeatherApi)
-        
-// }
-
-// fetch('api/foo?get_max=True')
-// .then( response => response.json())
-// .then( response => {
-//     var max = response["max"];
-//     return fetch('api2/bar?max=' + max)
-// })
-// .then( response => response.json())
-// .then( mydata => processdata(mydata))
