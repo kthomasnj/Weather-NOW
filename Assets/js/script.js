4436d694e038e0ff8fdb62ff6040237b5a4ae1c6
Keith S Thomas <kthomasnj@hotmail.com>
2022-08-13 14:27:40 -0400

    Adding png icon for 5-day weather.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,7 +22,6 @@ submitBtn.addEventListener('click', function () {
         .then(function (response) {
             return response.json();
         }).then(function (data) {
-
             cityMain.textContent = data[0].name + ", " + data[0].state;
             cityLat.setAttribute("value", data[0].lat);
             cityLat.setAttribute("lat", data[0].lat);
@@ -39,7 +38,6 @@ submitBtn.addEventListener('click', function () {
            .then(function (response) {
            return response.json();
         }).then(function (data) {
-            console.log(data.list[0]);
             var weatherCond1El = document.querySelector("#weather-cond");
             var weatherTemp1El = document.querySelector("#city-temp");
             var weatherWind1El = document.querySelector("#city-wind");
@@ -58,15 +56,16 @@ submitBtn.addEventListener('click', function () {
             //Day 1
 
             var day1DateEl = document.querySelector("#day-1 h2");
-            var day1IconEl = document.querySelector("#day-1 div i");
+            var day1IconEl = document.querySelector("#day-1 div img");
             var day1TempEl = document.querySelector("#one-temp");
             var day1WindEl = document.querySelector("#one-wind");
             var day1HumidEl = document.querySelector("#one-humid");
             var day1Day = data.list[0].dt_txt;
             var day1Conv = moment(day1Day).format("dddd");
+            var day1Icon = day1IconEl.textContent = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
         
-            day1DateEl.textContent = day1Conv;
-            day1IconEl.textContent = "icon: " + data.list[0].weather[0].icon;
+            day1IconEl.setAttribute("src", day1Icon);
+            day1DateEl.textContent = day1Conv; 
             day1TempEl.textContent = "Temperture: " + data.list[0].main.temp;
             day1WindEl.textContent = "Wind: " + data.list[0].wind.speed;
             day1HumidEl.textContent = "Humidity: " + data.list[0].main.humidity;
@@ -74,15 +73,17 @@ submitBtn.addEventListener('click', function () {
             //Day 2
 
             var day1DateEl = document.querySelector("#day-2 h2");
-            var day1IconEl = document.querySelector("#day-2 div i");
+            var day2IconEl = document.querySelector("#day-2 div img");
             var day1TempEl = document.querySelector("#two-temp");
             var day1WindEl = document.querySelector("#two-wind");
             var day1HumidEl = document.querySelector("#two-humid");
             var day2Day = data.list[9].dt_txt;
             var day2Conv = moment(day2Day).format("dddd");
+            var day2Icon = day2IconEl.textContent = "http://openweathermap.org/img/wn/" + data.list[9].weather[0].icon + "@2x.png";
 
+        
+            day2IconEl.setAttribute("src", day2Icon);
             day1DateEl.textContent = day2Conv;
-            day1IconEl.textContent = "icon: " + data.list[9].weather[0].icon;
             day1TempEl.textContent = "Temperture: " + data.list[9].main.temp;
             day1WindEl.textContent = "Wind: " + data.list[9].wind.speed;
             day1HumidEl.textContent = "Humidity: " + data.list[9].main.humidity;
@@ -90,15 +91,16 @@ submitBtn.addEventListener('click', function () {
             //Day 3
 
             var day1DateEl = document.querySelector("#day-3 h2");
-            var day1IconEl = document.querySelector("#day-3 div i");
+            var day1IconEl = document.querySelector("#day-3 div img");
             var day1TempEl = document.querySelector("#three-temp");
             var day1WindEl = document.querySelector("#three-wind");
             var day1HumidEl = document.querySelector("#three-humid");
             var day3Day = data.list[18].dt_txt;
             var day3Conv = moment(day3Day).format("dddd");
-
+            var day3Icon = day1IconEl.textContent = "http://openweathermap.org/img/wn/" + data.list[18].weather[0].icon + "@2x.png";
+        
+            day1IconEl.setAttribute("src", day3Icon);
             day1DateEl.textContent = day3Conv;
-            day1IconEl.textContent = "icon: " + data.list[18].weather[0].icon;
             day1TempEl.textContent = "Temperture: " + data.list[18].main.temp;
             day1WindEl.textContent = "Wind: " + data.list[18].wind.speed;
             day1HumidEl.textContent = "Humidity: " + data.list[18].main.humidity;
@@ -106,15 +108,16 @@ submitBtn.addEventListener('click', function () {
             //Day 4
 
             var day1DateEl = document.querySelector("#day-4 h2");
-            var day1IconEl = document.querySelector("#day-4 div i");
+            var day1IconEl = document.querySelector("#day-4 div img");
             var day1TempEl = document.querySelector("#four-temp");
             var day1WindEl = document.querySelector("#four-wind");
             var day1HumidEl = document.querySelector("#four-humid");
             var day4Day = data.list[27].dt_txt;
             var day4Conv = moment(day4Day).format("dddd");
-
+            var day4Icon = day1IconEl.textContent = "http://openweathermap.org/img/wn/" + data.list[27].weather[0].icon + "@2x.png";
+        
+            day1IconEl.setAttribute("src", day4Icon);
             day1DateEl.textContent = day4Conv;
-            day1IconEl.textContent = "icon: " + data.list[27].weather[0].icon;
             day1TempEl.textContent = "Temperture: " + data.list[27].main.temp;
             day1WindEl.textContent = "Wind: " + data.list[27].wind.speed;
             day1HumidEl.textContent = "Humidity: " + data.list[27].main.humidity;
@@ -122,20 +125,20 @@ submitBtn.addEventListener('click', function () {
             //Day 5
 
             var day1DateEl = document.querySelector("#day-5 h2");
-            var day1IconEl = document.querySelector("#day-5 div i");
+            var day1IconEl = document.querySelector("#day-5 div img");
             var day1TempEl = document.querySelector("#five-temp");
             var day1WindEl = document.querySelector("#five-wind");
             var day1HumidEl = document.querySelector("#five-humid");
             var day5Day = data.list[36].dt_txt;
             var day5Conv = moment(day5Day).format("dddd");
-
+            var day5Icon = day1IconEl.textContent = "http://openweathermap.org/img/wn/" + data.list[36].weather[0].icon + "@2x.png";
+        
+            day1IconEl.setAttribute("src", day5Icon);
             day1DateEl.textContent = day5Conv;
-            day1IconEl.textContent = "icon: " + data.list[36].weather[0].icon;
             day1TempEl.textContent = "Temperture: " + data.list[36].main.temp;
             day1WindEl.textContent = "Wind: " + data.list[36].wind.speed;
             day1HumidEl.textContent = "Humidity: " + data.list[36].main.humidity;
         });
-
 });
 
 timeEl.textContent = currentTime;
